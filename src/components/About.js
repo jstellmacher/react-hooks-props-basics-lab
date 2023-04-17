64f4a3215bb27a99f308599bbf029adda8497137
Jai <jaijai.stell@gmail.com>
2023-04-17 10:35:15 -0700

    completes lab in a destructured way

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,21 @@
 import React from "react";
+import Links from "./Links"
 
-function About() {
+function About({bio, github, linkedin}) {
+  //! destructured
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      
+      {/* condition ? if : else */}
+      {!bio || bio === "" ? null : <p>{bio}</p>}
+
+      {/* || this is the logical OR operator this means true only if both expressions are true, otherwise false...! bio = the NOT operator returns true for a false expression and false for a true expression */}
+      {/* Left side: !bio is not passed down || or bio is empty if null else you put bio prop into <p> tag  */}
+      
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      {/* !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! */}
+      {<Links github={github} linkedin={linkedin} />}
     </div>
   );
 }
